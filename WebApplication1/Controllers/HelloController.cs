2a0c0958d732cd5dd9d87977142fe9b0249d0761
Praddy18 <prasadsatam29@gmail.com>
2023-05-08 00:44:20 +0530

    first commit

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Student()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Prasad",
+                Gender = "Male"
+            };
+
+            return View(student);
+         }
+
+    }
+}
